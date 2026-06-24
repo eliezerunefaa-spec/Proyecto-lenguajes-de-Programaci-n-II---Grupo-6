5289f8dc9ad23be50ddb24325039a60bba12ecc1
Eliezer Padrón <eliezerunefaa@gmail.com>
2026-06-24 10:50:42 -0400

    Creando el entorno para el proyecto

---
+++ integrantes.md
@@ -0,0 +1,7 @@
+1.Eliezer Oswaldo Padron 
+2.Angely Diaz
+3.Jenniret Gonzalez 
+4.Odalys Escalona 
+5.Xavier Tierado 
+6.Flor Mota 
+7.Diego Alvarado
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std;
+int main () {
+string texto;
+
+cout << "=== PROBANDO EL DOCKER ===" <<endl;
+cout << "ingrese su grupo: ";
+cin >> texto;
+cout << "Muy bien, el " << texto << " esta esjecutando desde un contenedor linux y funciona. "<< endl;
+return 0;
+
+
+
+}
